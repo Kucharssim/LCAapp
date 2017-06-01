a3aa6662cee6cdd68006f184a8437f4c25cd8e2e
Simon Kucharsky <kucharssim@gmail.com>
2017-06-01 16:41:53 +0200

    Update global.R
    Install missing packages

--- global.R
+++ global.R
@@ -1,3 +1,10 @@
+if(!require('parallel')){install.packages('parallel')}
+if(!require('gtools')){install.packages('gtools')}
+if(!require('DT')){install.packages('DT')}
+if(!require('reshape2')){install.packages('reshape2')}
+if(!require('dplyr')){install.packages('dplyr')}
+if(!require('plotly')){install.packages('plotly')}
+
 library(shiny)
 library(parallel)
 library(gtools)
